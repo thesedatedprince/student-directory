31d57bbcd8f8d853a2ee1d7f86271c71a86d8607
Matthew <thesedatedprince@gmail.com>
2016-12-06 06:27:28 +0000

    Updated directory with student count

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
+#First, we print the list of students
 puts "The students of Villains Academy"
-"------------"
+puts "------------"
 puts "Lord Voldemort"
 puts "Doctor Robotnik"
 puts "Freddy Vorhees"
@@ -11,3 +12,8 @@ puts "Patrik Bateman"
 puts "Robert Killgrave"
 puts "Uchiha Sasuke"
 puts "Yagami Light"
+# next, we print the total number of students
+print "Overall, we have "
+# note that print() doesn't add new line characters
+print 11
+puts " great students."
