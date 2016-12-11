f135726fc27de1aedd8c501a5c9b5979e6d7b869
Matthew <thesedatedprince@gmail.com>
2016-12-11 17:24:42 +0000

    Directory exercise 11 and gitignore file

--- directory.rb
+++ directory.rb
@@ -59,13 +59,12 @@ def input_students
     puts "Now enter #{name}'s height."
     height = gets.chomp
     end
-  #And we return the array of students
-
   end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -81,6 +80,8 @@ def process(selection)
     students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -139,6 +140,17 @@ end
 def print_footer
 puts "Overall, we have #{@students.count} great students."
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 # now, we call the methods
 
 interactive_menu
