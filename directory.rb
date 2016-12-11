489b7ee6fd1620345b25072c35f8a3fc9b571c5a
Matthew <thesedatedprince@gmail.com>
2016-12-11 17:53:29 +0000

    Removed previous exercises from file to do next ones - check previous commit for sec 8 exercises

--- directory.rb
+++ directory.rb
@@ -4,60 +4,13 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice."
   #first, we ask for the first name
-  name = gets.chop
-  puts "Now enter #{name}'s cohort"
-  cohort = gets.chop
-  if cohort == ''
-    cohort = 'November'
-  end
-  until cohort.capitalize == 'January' || cohort.capitalize == 'February' ||
-    cohort.capitalize == 'March' || cohort.capitalize == 'April' ||
-    cohort.capitalize == 'May' || cohort.capitalize == 'June' ||
-    cohort.capitalize == 'July' || cohort.capitalize =='August' ||
-    cohort.capitalize == 'September' || cohort.capitalize =='October' ||
-    cohort.capitalize =='November' || cohort.capitalize == 'December'
-    puts "I think you made a typo. Please re-enter"
-    cohort = gets.chop
-  end
-  puts "Now enter #{name}'s hobby."
-  hobby = gets.chop
-  puts "Now enter #{name}'s country of birth."
-  country = gets.chop
-  puts "Now enter #{name}'s height."
-  height = gets.chop
-  # While name is not empty, we repeat this code
+  name = STDIN.gets.chomp
   while !name.empty? do
     #add the student hash to the array
-    @students << {name: name, cohort: cohort, hobby: hobby, country: country,
-                height: height}
+    @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students."
     # Now we ask for another name from the user
-    puts "Please type in the next student or hit enter to finish"
-    name = gets.chomp
-    if name == ''
-      break
-    end
-    puts "Now enter #{name}'s cohort"
-    cohort = ''
-    cohort = gets.chomp
-    if cohort == ''
-      cohort = 'November'
-    end
-    until cohort.capitalize == 'January' || cohort.capitalize == 'February' ||
-      cohort.capitalize == 'March' || cohort.capitalize == 'April' ||
-      cohort.capitalize == 'May' || cohort.capitalize == 'June' ||
-      cohort.capitalize == 'July' || cohort.capitalize =='August' ||
-      cohort.capitalize == 'September' || cohort.capitalize =='October' ||
-      cohort.capitalize =='November' || cohort.capitalize == 'December'
-      puts "I think you made a typo. Please re-enter"
-      cohort = gets.chomp
-    end
-    puts "Now enter #{name}'s hobby"
-    hobby = gets.chomp
-    puts "Now enter #{name}'s country of birth"
-    country = gets.chomp
-    puts "Now enter #{name}'s height."
-    height = gets.chomp
+    name = STDIN.gets.chomp
     end
   end
 
@@ -96,7 +49,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -105,37 +58,11 @@ def print_header
 puts "------------"
 end
 
-=begin
 
-commented out to rewrite method using while loop
-
-def print(students)
-students.each_with_index.select do |student, index|
-  if student[:name].downcase.start_with?('h') && student[:name].length <= 12
-  puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-end
-=end
 
 def print_students_list
-  if @students.length == 0
-    puts "There are no students."
-  else
-
-  @students.sort!{|a, b| a[:cohort]<=>b[:cohort]}
-  count = 0
-  until count == @students.length
-
 @students.each do |student|
-  puts "#{student[:name]}
-  (#{student[:cohort]} cohort)
-  (Hobbies: #{student[:hobby]})
-  (Country of Birth: #{student[:country]})
-  (Height: #{student[:height]})".center(50)
-  count += 1
-end
-end
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
@@ -154,8 +81,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -163,6 +90,17 @@ def load_students
   file.close
 end
 
-# now, we call the methods
-
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+  # now, we call the methods
+try_load_students
 interactive_menu
